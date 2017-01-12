2420a0943329d685be6aa88ffca3fd719a4b7117
Christian Burkhart <burkhart.christian@gmail.com>
2017-01-12 21:35:52 +0100

    refactor code

--- web/mysite/urls.py
+++ web/mysite/urls.py
@@ -1,4 +1,4 @@
-"""myapp URL Configuration
+"""mysite URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.10/topics/http/urls/
