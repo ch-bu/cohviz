b7617acefbb27393d9809438dd736e382986cd1e
Christian Burkhart <burkhart.christian@gmail.com>
2017-01-21 12:09:25 +0100

    add environmental variables, django is running and reloading instantly

--- web/mywebapp/models.py
+++ web/mywebapp/models.py
@@ -3,3 +3,5 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class TestModel(models.Model):
+    first_name = models.CharField(max_length=30)
--- web/mywebapp/views.py
+++ web/mywebapp/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello world. You are at my apps")
+    return HttpResponse("<b>Hello world. You are at myapp</b>")
 
 # Create your views here.
