6bd117e11699c1570d01af6669f3754a59784e8d
Christian Burkhart <burkhart.christian@gmail.com>
2017-06-22 16:15:03 +0200

    feat: fetch user data for integrated treatment

--- web/cohapp/static/cohapp/js_big/components/jsx-strings.jsx
+++ web/cohapp/static/cohapp/js_big/components/jsx-strings.jsx
@@ -9,4 +9,36 @@ function getInstruction() {
       </div>;
 }
 
-export {getInstruction};
+var my_urls = function urls() {
+
+    var container = {};
+
+    container.url =  window.location.href;
+    container.url_split = container.url.split('/');
+
+    container.url = window.location.href;
+    container.split = container.url.split('/');
+    container.website = container.split[0] + '//' + container.split[2];
+    container.dashboard = container.website + "/dashboard/";
+
+    // Api endpoints
+    container.experiments = container.website + '/apis/experiments/';
+    container.measurement = container.website + '/apis/measurements/';
+    container.experiment = container.website + '/apis/experiment/';
+    container.user_specific = container.website + '/apis/user-specific/';
+    container.user_experiment = container.website + '/apis/user-experiment';
+    container.user_specific_name = container.website + '/apis/user-specific-name/';
+    container.user = container.website + '/apis/user-experiment/';
+    container.registration = container.website + '/apis/registration/';
+    container.groups = container.website + '/apis/groups/';
+    container.textanalyzer = container.website + '/apis/textanalyzer/';
+    container.textdata = container.website + '/apis/textdata/';
+    container.csv_text_data = container.website + '/csv_text_export/';
+
+    container.single_experiment = container.website + '/experiment/';
+    container.run_experiment = container.website + '/run-experiment/';
+
+    return container;
+}();
+
+export {getInstruction, my_urls};
--- web/cohapp/static/cohapp/js_big/treatments/integrated.jsx
+++ web/cohapp/static/cohapp/js_big/treatments/integrated.jsx
@@ -1,7 +1,37 @@
-import {getInstruction} from '../components/jsx-strings.jsx';
+import {getInstruction, my_urls} from '../components/jsx-strings.jsx';
 
 class TreatmentIntegrated extends React.Component {
+  constructor(props) {
+    super(props);
+
+    // Set initial state
+    this.state = {
+      data: null
+    };
+
+    var self = this;
+
+    // Get urls
+    var urls = my_urls;
+
+    // Get hash of experiment
+    var path = window.location.href;
+    this.experiment_id = path.substr(path.lastIndexOf('/') + 1);
+
+    // Fetch user data
+    fetch(urls.user_specific + this.experiment_id, {
+      method: 'GET',
+      credentials: "same-origin"
+    }).then(function(response) {
+      return response.json();
+    }).then(function(data) {
+      self.setState({data: data});
+    });
+
+  }
+
   render() {
+    console.log(this.state);
     // Get jsx string of instruction
     let instruction = getInstruction();
 
