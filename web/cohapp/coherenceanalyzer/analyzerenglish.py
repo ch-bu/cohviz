e090513bc37ebd92c5927f93f3dcc104d79d5b9e
Christian Burkhart <burkhart.christian@gmail.com>
2017-08-11 16:03:19 +0200

    feat: minor changes in nouns; bug in get_clusters

--- web/cohapp/coherenceanalyzer/analyzerenglish.py
+++ web/cohapp/coherenceanalyzer/analyzerenglish.py
@@ -23,6 +23,7 @@ class CohesionAnalyzerEnglish:
         # Remove parenthesis in the whole text
         text_nlp = text.replace('[LINEBREAK]', '')
         text_nlp = re.sub("([\(\[]).*?([\)\]])", "", text_nlp)
+        text_nlp = re.sub("'\"", "", text_nlp)
         text_nlp = re.sub('\n', ' ', text_nlp)
 
         # Prepare text and remove unwanted characters
@@ -31,7 +32,7 @@ class CohesionAnalyzerEnglish:
         # Extract sentences
         sentences = [sent for sent in text.sents]
 
-        return text_nlp, sentences, paragraphs,
+        return text_nlp, sentences, paragraphs
 
 
     def _generate_nouns(self, sentences):
@@ -42,18 +43,18 @@ class CohesionAnalyzerEnglish:
         subjects = []
         word_dict = {}
 
-        import time
         start_time = time.time()
 
 
         for index, sentence in enumerate(sentences):
             # Get noun chunks
-            noun_chunks = list(sentence.noun_chunks)
+            noun_chunks = filter(lambda x: x.root.prob < -7, list(sentence.noun_chunks))
+
+            # print [(token, token.root.prob) for token in noun_chunks]
 
             try:
                 # Get first subject
                 subject = [sub for sub in noun_chunks if any(sub.root.dep_ for s in ['nsubj', 'csubj', 'nsubjpass'])][0]
-                # print (subject, subject.root.dep_, subject.root.pos_)
                 # Append subject to list
                 subjects.append(subject)
             except:
@@ -66,32 +67,35 @@ class CohesionAnalyzerEnglish:
                     word_dict[noun_chunks[0].root.lemma_] = noun_chunks[0].orth_
 
                 # Append
-                word_pairs.append({'source': word_dict[noun_chunks[0].root.lemma_],
-                                   'target': word_dict[noun_chunks[0].root.lemma_],
-                                   'device': 'within'})
-            # We have a subject
+                word_pairs.append(
+                  {'source': word_dict[noun_chunks[0].root.lemma_],
+                   'target': word_dict[noun_chunks[0].root.lemma_],
+                   'device': 'within'})
+            # There is a subject in the current sentence
             elif subject:
                 # Subject not a pronoun
-                if subject.root.dep_ != 'PRON':
+                if subject.root.pos_ != 'PRON':
                     # Combine subject with noun_chunks
                     for chunk in noun_chunks:
                         # Do not combine the same chunk
                         if chunk.orth_ != subject.orth_ \
-                            and chunk.root.pos_ != 'PRON' \
-                            and subject.root.pos_ != 'PRON':
+                            and chunk.root.pos_ != 'PRON':
                             # We already stored a subject with the same root
                             if subject.root.lemma_ in word_dict:
                                 # We alredy stored the noun chunk
                                 if chunk.root.lemma_ in word_dict:
-                                    word_pairs.append({'source': word_dict[subject.root.lemma_],
-                                                       'target': word_dict[chunk.root.lemma_],
-                                                       'device': 'within'})
+                                    word_pairs.append(
+                                      {'source': word_dict[subject.root.lemma_],
+                                       'target': word_dict[chunk.root.lemma_],
+                                       'device': 'within'})
                                 # The noun chunk is new to us
                                 else:
+                                    # Store word in dict
                                     word_dict[chunk.root.lemma_] = chunk.orth_
-                                    word_pairs.append({'source': word_dict[subject.root.lemma_],
-                                                       'target': chunk.orth_,
-                                                       'device': 'within'})
+                                    word_pairs.append(
+                                      {'source': word_dict[subject.root.lemma_],
+                                       'target': chunk.orth_,
+                                       'device': 'within'})
                             # We haven't stored the current subject
                             else:
                                 word_dict[subject.root.lemma_] = subject.orth_
@@ -135,7 +139,7 @@ class CohesionAnalyzerEnglish:
                                         word_pairs.append({'source': comb[0].orth_,
                                                            'target': comb[1].orth_,
                                                            'device': 'between'})
-            # If there is no subject combine every pair
+            # No subject is in the current sentence
             elif not subject:
                 no_sub_combinations = combinations(noun_chunks, 2)
                 # There are combinations
@@ -162,62 +166,55 @@ class CohesionAnalyzerEnglish:
                                                    'device': 'between'})
 
 
-            # # Lets look at the next sentence if there is a link between the two
-            # if index < (len(sentences) - 1):
-            #     # Get noun chunks of next sentence
-            #     noun_chunks_next = list(sentences[index + 1].noun_chunks)
+            # Lets look at the next sentence if there is a link between the two
+            if index < (len(sentences) - 1):
+                # Get noun chunks of next sentence
+                noun_chunks_next = list(sentences[index + 1].noun_chunks)
 
-            #     # Combine all chunks between two sentences
-            #     my_combinations = list(list(zip(r, p)) for (r, p) in zip(repeat(noun_chunks), permutations(noun_chunks_next)))
+                # Combine all chunks between two sentences
+                my_combinations = list(list(zip(r, p)) for (r, p) in zip(repeat(noun_chunks), permutations(noun_chunks_next)))
 
-            #     # Calculate similarity between pairs
-            #     similarity_pairs = [(pair[0], pair[1], pair[0].similarity(pair[1])) for comb in my_combinations for pair in comb]
 
-            #     # We are only interested in pairs with a high similarity
-            #     similarity_filter = filter(lambda x: x[2] > .72, similarity_pairs)
+                # Calculate similarity between pairs
+                similarity_pairs = [(pair[0], pair[1], pair[0].similarity(pair[1])) for comb in my_combinations for pair in comb]
 
-            #     # We have found chunks that are similar
-            #     if similarity_filter:
-            #         # Loop over every pair and append
-            #         for pair in similarity_filter:
-            #             if pair[0].orth_ != pair[1].orth_:
-            #                 if pair[0].root.lemma_ in word_dict:
-            #                     if pair[1].root.lemma_ in word_dict:
-            #                         word_pairs.append({'source': word_dict[pair[0].root.lemma_],
-            #                                            'target': word_dict[pair[1].root.lemma_],
-            #                                            'device': 'between'})
+                # We are only interested in pairs with a high similarity
+                similarity_filter = filter(lambda x: x[2] > .76, similarity_pairs)
 
-            #                     else:
-            #                         word_dict[pair[1].root.lemma_] = pair[1].orth_
-            #                         word_pairs.append({'source': word_dict[pair[0].root.lemma_],
-            #                                            'target': pair[1].orth_,
-            #                                            'device': 'between'})
-            #                 else:
-            #                     word_dict[pair[0].root.lemma_] = pair[0].orth_
-            #                     word_pairs.append({'source': pair[0].orth_,
-            #                                        'target': pair[1].orth_,
-            #                                        'device': 'between'})
-        print("--- %s seconds ---" % (time.time() - start_time))
-        # word_pairs = [{'source': 'Hans', 'target': 'Haus', 'device': 'within'}]
+                # We have found chunks that are similar
+                if len(similarity_filter) > 0:
+                    # Loop over every pair and append
+                    for pair in similarity_filter:
+                        # Do not add pairs with same orthographie
+                        if pair[0].orth_ != pair[1].orth_:
+                            # Check if root already exists
+                            if pair[0].root.lemma_ in word_dict:
+                                if not pair[1].root.lemma_ in word_dict:
+                                    word_dict[pair[1].root.lemma_] = pair[1].orth_
+                            else:
+                                if not pair[1].root.lemma_ in word_dict:
+                                    word_dict[pair[1].root.lemma_] = pair[1].orth_
+
+                                word_dict[pair[0].root.lemma_] = pair[0].orth_
+
+                            # Append word pairs
+                            word_pairs.append(
+                              {'source': word_dict[pair[0].root.lemma_],
+                               'target': word_dict[pair[1].root.lemma_],
+                               'device': 'between'})
+
+        print("--- Nouns: %s seconds ---" % (time.time() - start_time))
+
         return word_pairs, subjects
-
-
-    def _calculate_number_relations(self):
-        """Calculates the number of relations"""
-
-        # Make tuples from word_pairs
-        tuples = map(lambda x: (x['source'], x['target']), self.word_pairs)
-
-        # Remove duplicates
-        tuples = list(set([(pair[0], pair[1])
-            for pair in tuples if pair[0] != pair[1]]))
-
-        return len(tuples)
 
 
     def _get_clusters(self, sentences, word_pairs):
         """Calculates the number of computed
         clusters"""
+
+
+        start_time = time.time()
+        print '--- Start clusters ---'
 
         # If we only have one sentence return word pairs
         # as single cluster
@@ -308,6 +305,8 @@ class CohesionAnalyzerEnglish:
                 # Append current cluster to all clusters
                 clusters.append(current_cluster)
 
+        print("--- Number Clusters: %s seconds ---" % (time.time() - start_time))
+
         return clusters
 
 
@@ -315,6 +314,8 @@ class CohesionAnalyzerEnglish:
         """Generate a dictionary that
         has the words as key and the cluster number
         as value"""
+
+        start_time = time.time()
 
         # When clusters are calculated assign them to the word_pairs as
         # an additional value
@@ -333,6 +334,8 @@ class CohesionAnalyzerEnglish:
             for word in words:
                 word_cluster_index[word] = index
 
+        print("--- Number Cluster Index: %s seconds ---" % (time.time() - start_time))
+
         return word_cluster_index
 
 
@@ -347,6 +350,9 @@ class CohesionAnalyzerEnglish:
         Returns:
             String - An html formatted string
         """
+
+        start_time = time.time()
+
         # Init string to return
         html_string = ''
 
@@ -398,6 +404,7 @@ class CohesionAnalyzerEnglish:
             # Change to span element
             html_string = html_string.replace(node, '<span class="cluster-' + str(cluster) + '">' + node + '</span>')
 
+        print("--- Html Index: %s seconds ---" % (time.time() - start_time))
 
         return html_string
 
@@ -405,12 +412,16 @@ class CohesionAnalyzerEnglish:
     def _calculate_number_relations(self, word_pairs):
         """Calculates the number of relations"""
 
+        start_time = time.time()
+
         # Make tuples from word_pairs
         tuples = map(lambda x: (x['source'], x['target']), word_pairs)
 
         # Remove duplicates
         tuples = list(set([(pair[0], pair[1])
             for pair in tuples if pair[0] != pair[1]]))
+
+        print("--- Number Relations Index: %s seconds ---" % (time.time() - start_time))
 
         return len(tuples)
 
